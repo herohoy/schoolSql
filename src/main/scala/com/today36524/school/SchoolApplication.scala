2b44c0671cb4075b28ff3465c37a3977ab8e29f7
lihui <herohoy@icloud.com>
2017-12-16 09:51:31 +0800

    create test3 code

---
+++ src/main/scala/com/today36524/school/SchoolApplication.scala
@@ -0,0 +1,18 @@
+package com.today36524.school
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.{RestController,RequestMapping};
+
+object SchoolApplication {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[SchoolApplication], args.toString)
+  }
+}
+
+@SpringBootApplication
+@RestController
+class SchoolApplication{
+  @RequestMapping(Array("/hello"))
+  def hello:String = "<h2>Hello World!</h2>"
+}
